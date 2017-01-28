806e930d3d2e2117561a96ea946871e043714d47
Steph-6 <Steph.robinson95@gmail.com>
2017-01-28 15:38:50 +0000

    tokens storing to local storage for login and register

--- NapApp/src/js/controllers/login.controller.js
+++ NapApp/src/js/controllers/login.controller.js
@@ -2,17 +2,18 @@ angular
   .module('napApp')
   .controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['User'];
-function LoginCtrl(User) {
+LoginCtrl.$inject = ['User', 'TokenService'];
+function LoginCtrl(User, TokenService) {
   const vm = this;
 
   vm.login = () => {
     User.login(vm.user)
       .$promise
       .then(data => {
-        console.log(data)
+        console.log(data);
+        TokenService.setToken(data.token);
       }, err => {
-        console.log(err)
+        console.log(err);
       });
   };
 }
--- NapApp/src/js/controllers/register.controller.js
+++ NapApp/src/js/controllers/register.controller.js
@@ -2,8 +2,8 @@ angular
   .module('napApp')
   .controller('RegisterCtrl', RegisterCtrl);
 
-RegisterCtrl.$inject = ['User'];
-function RegisterCtrl(User){
+RegisterCtrl.$inject = ['User', 'TokenService'];
+function RegisterCtrl(User, TokenService){
   const vm = this;
 
   vm.register = () => {
@@ -13,6 +13,7 @@ function RegisterCtrl(User){
       .$promise
       .then(data => {
         console.log(data);
+        TokenService.setToken(data.token);
       }, err => {
         console.log(err);
       });
