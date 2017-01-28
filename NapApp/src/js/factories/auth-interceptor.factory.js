6d0decf64eb6e6302344f6ae21c41b60be7e5c41
Steph-6 <Steph.robinson95@gmail.com>
2017-01-28 16:01:57 +0000

    get token function added

--- NapApp/src/js/factories/auth-interceptor.factory.js
+++ NapApp/src/js/factories/auth-interceptor.factory.js
@@ -6,11 +6,11 @@ AuthInterceptor.$inject = ['API', 'TokenService'];
 function AuthInterceptor(API, TokenService) {
   return {
     request: function(config) {
-      console.log(config);
+      const token = TokenService.getToken();
+      console.log(token);
       return config;
     },
     response: function(res) {
-      console.log(res);
       if (res.config.url.indexOf(API) === 0 && res.data.token) {
         TokenService.setToken(res.data.token);
       }
