295b471142564a1617cebfd5b9db101c53255055
Steph-6 <Steph.robinson95@gmail.com>
2017-01-28 16:35:14 +0000

    logout added

--- NapApp/src/js/controllers/main.controller.js
+++ NapApp/src/js/controllers/main.controller.js
@@ -2,13 +2,18 @@ angular
   .module('napApp')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['$rootScope', 'CurrentUserService'];
-function MainCtrl($rootScope, CurrentUserService) {
+MainCtrl.$inject = ['$rootScope', 'CurrentUserService', '$state'];
+function MainCtrl($rootScope, CurrentUserService, $state) {
   const vm = this;
 
   $rootScope.$on('loggedIn', () => {
     vm.user = CurrentUserService.currentUser;
     console.log(vm.user);
+  });
+
+  $rootScope.$on('loggedOut', () => {
+    vm.user = null;
+    $state.go('login');
   });
 
   vm.logout = () => {
