35e8b277c6dc67a56688ec136a49e309da7091dd
Steph-6 <Steph.robinson95@gmail.com>
2017-01-28 15:48:13 +0000

    authentication interceptor added

---
+++ NapApp/src/js/factories/auth-interceptor.factory.js
@@ -0,0 +1,17 @@
+angular
+  .module('napApp')
+  .factory('AuthInterceptor', AuthInterceptor);
+
+AuthInterceptor.$inject = [];
+function AuthInterceptor() {
+  return {
+    request: function(config) {
+      console.log(config);
+      return config;
+    },
+    response: function(res) {
+      console.log(res);
+      return res;
+    }
+  };
+}
