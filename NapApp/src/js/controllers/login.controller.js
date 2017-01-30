ea9ff08d013510d1ffdc12dff9a9a8f2f21f3d55
edcompton <eahcompton@gmail.com>
2017-01-30 18:07:02 +0000

    added wkr orking authentication

--- NapApp/src/js/controllers/login.controller.js
+++ NapApp/src/js/controllers/login.controller.js
@@ -1,19 +1,30 @@
 angular
-  .module('napApp')
-  .controller('LoginCtrl', LoginCtrl);
+.module('napApp')
+.controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['User','CurrentUserService'];
-function LoginCtrl(User, CurrentUserService) {
+LoginCtrl.$inject = ['User','CurrentUserService', '$auth'];
+function LoginCtrl(User, CurrentUserService, $auth) {
   const vm = this;
+
+  vm.authenticate = function(provider) {
+    $auth.authenticate(provider)
+    .then(function(res) {
+      console.log( 'success', res);
+    })
+    .catch(function(res) {
+      console.log( 'error', res);
+    });
+  };
+
 
   vm.login = () => {
     User
-      .login(vm.user)
-      .$promise
-      .then(() => {
-        CurrentUserService.getUser();
-      }, err => {
-        console.log(err);
-      });
+    .login(vm.user)
+    .$promise
+    .then(() => {
+      CurrentUserService.getUser();
+    }, err => {
+      console.log(err);
+    });
   };
 }
