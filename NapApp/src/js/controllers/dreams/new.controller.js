cc359bab8d480342b6301ba271498a69fbd8c086
Steph-6 <Steph.robinson95@gmail.com>
2017-01-30 13:16:44 +0000

    user welcomed and form added

--- NapApp/src/js/controllers/dreams/new.controller.js
+++ NapApp/src/js/controllers/dreams/new.controller.js
@@ -2,10 +2,17 @@ angular
   .module('napApp')
   .controller('DreamsNewCtrl', DreamsNewCtrl);
 
-DreamsNewCtrl.$inject = ['$http', 'API', '$state'];
-function DreamsNewCtrl() {
+DreamsNewCtrl.$inject = ['$http', 'API', '$state','User','CurrentUserService'];
+function DreamsNewCtrl(http, API, $state, User, CurrentUserService) {
   const vm  = this;
+  vm.user = CurrentUserService.currentUser;
   vm.dreamsCreate = function dreamsCreate(){
     
-  }
+  };
 }
+//
+// return $http
+//       .post(`${API}/users`, vm.user)
+//       .then(() => {
+//         $state.go('usersIndex');
+//       });
