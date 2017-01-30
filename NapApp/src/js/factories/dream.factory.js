ef34b6bd2f4cace5ce8547c4a29ddc9ab53504d3
Steph-6 <Steph.robinson95@gmail.com>
2017-01-30 12:13:26 +0000

    dream factory made

---
+++ NapApp/src/js/factories/dream.factory.js
@@ -0,0 +1,10 @@
+angular
+  .module('napApp')
+  .factory('Dream', dreamFactory);
+
+dreamFactory.$inject = ['API', '$resource'];
+function dreamFactory(API, $resource){
+  return $resource(`${API}/users/:id/dreams`, { id: '@_id'}, {
+    'new': { method: 'POST', url: `${API}/users` }
+  });
+}
