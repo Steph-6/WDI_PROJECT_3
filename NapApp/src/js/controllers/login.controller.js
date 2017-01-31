29f11f5d72cd84a41c548a3cb50e7adffcb510ac
edcompton <eahcompton@gmail.com>
2017-01-31 11:55:35 +0000

    got API data returned from fitbit API

--- NapApp/src/js/controllers/login.controller.js
+++ NapApp/src/js/controllers/login.controller.js
@@ -2,20 +2,20 @@ angular
 .module('napApp')
 .controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['User','CurrentUserService', '$auth'];
-function LoginCtrl(User, CurrentUserService, $auth) {
+LoginCtrl.$inject = ['User', 'CurrentUserService', '$auth', '$http'];
+function LoginCtrl(User, CurrentUserService, $auth, $http) {
   const vm = this;
 
   vm.authenticate = function(provider) {
     $auth.authenticate(provider)
     .then(function(res) {
       console.log( 'success', res);
+      vm.getFitbitData(res);
     })
     .catch(function(res) {
       console.log( 'error', res);
     });
   };
-
 
   vm.login = () => {
     User
@@ -25,6 +25,17 @@ function LoginCtrl(User, CurrentUserService, $auth) {
       CurrentUserService.getUser();
     }, err => {
       console.log(err);
+    });
+  };
+
+  vm.getFitbitData = (token) => {
+    vm.access = token.access_token;
+    $http
+    .get(`https://api.fitbit.com/1/user/-/sleep/date/2017-01-30.json`, {
+      headers: {'Authorization': `Bearer ${vm.access}`}
+    })
+    .then(response => {
+      console.log(response);
     });
   };
 }
