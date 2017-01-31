d704dfd2b94317446557059bd9130c5a0833e1f3
Steph-6 <Steph.robinson95@gmail.com>
2017-01-31 16:22:02 +0000

    delete added, fitbit data added t show

--- NapApp/src/js/controllers/dreams/show.controller.js
+++ NapApp/src/js/controllers/dreams/show.controller.js
@@ -2,15 +2,26 @@ angular
   .module('napApp')
   .controller('DreamsShowCtrl', DreamsShowCtrl);
 
-DreamsShowCtrl.$inject = ['CurrentUserService', 'Dream', '$stateParams'];
-function DreamsShowCtrl(CurrentUserService, Dream, $stateParams) {
+DreamsShowCtrl.$inject = ['CurrentUserService', 'Dream', '$stateParams', '$state'];
+function DreamsShowCtrl(CurrentUserService, Dream, $stateParams, $state) {
   const vm = this;
+  dreamsShow();
 
-  Dream
-    .get($stateParams)
-    .$promise
-    .then(data => {
-      vm.dream = data;
-    });
+  function dreamsShow() {
+    Dream
+      .get($stateParams)
+      .$promise
+      .then(data => {
+        vm.dream = data;
+      });
+  }
 
+  vm.delete = function dreamsDelete() {
+    Dream
+      .delete($stateParams)
+      .$promise
+      .then(() => {
+        $state.go('dreamsIndex');
+      });
+  };
 }
