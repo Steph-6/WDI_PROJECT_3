165242e97b9ad20412cc5205508f2be4e8ee1901
MustaphaMiah <muhammad_mustapha_miah@live.co.uk>
2017-03-07 19:29:34 +0000

    Adding environment variable for mongolab

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/nap-app',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/nap-app',
   secret: process.env.SECRET || 'gosh this is so secret... shhh...'
 };
